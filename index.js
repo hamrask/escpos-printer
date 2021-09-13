f33580c2f9f5efcb7765f2adda18db74083783d4
Fuad <fuadsanin221.fs@gmail.com>
2021-09-13 19:49:40 +0530

    printer project

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const receipt = require('receipt');
-
-receipt.config.currency = '₹'; // The currency symbol to use in output.
-receipt.config.width = 50;     // The amount of characters used to give the output a "width".
+receipt.config.currency = '';// The currency symbol to use in output.
+receipt.config.width = 55;     // The amount of characters used to give the output a "width".
 receipt.config.ruler = '=';    // The character used for ruler output.
 
-const output = receipt.create([
-    { type: 'text', value: [
-        'MY AWESOME STORE',
-        '123 STORE ST',
-        'store@store.com',
-        'www.store.com'
-    ], align: 'center' },
+generateBill = (orderNumber,billDate, items,userName,Total,printerIp,billHeader,billSubHeader1,billSubHeader2 ) =>{ 
+const firstPart = receipt.create([
+    /*{ type: 'text', value: [       
+        'Near Madeena Palli,   Chelakkad','  Mob:7898451355,8794561230'        
+    ], align: 'center' },*/
     { type: 'empty' },
-    { type: 'properties', lines: [
-        { name: 'Order Number', value: 'XXXXXXXXXXXX' },
-        { name: 'Date', value: 'XX/XX/XXXX XX:XX' }
-    ] },
-    { type: 'table', lines: [
-        { item: 'Product 1', qty: 1, cost: 1000 },
-        { item: 'Product 2 with a really long name', qty: 1, cost: 17500, discount: { type: 'absolute', value: 1000 } },
-        { item: 'Another product wth quite a name', qty: 2, cost: 900 },
-        { item: 'Product 4', qty: 1, cost: 80, discount: { type: 'percentage', value: 0.15 } },
-        { item: 'This length is ridiculously lengthy', qty: 14, cost: 8516 },
-        { item: 'Product 6', qty: 3, cost: 500 },
-        { item: 'Product 7', qty: 3, cost: 500, discount: { type: 'absolute', value: 500, message: '3 for the price of 2' } }
-    ] },
+    { type: 'properties',align:'left', lines: [
+        { name: 'Order Number', value: orderNumber },
+        { name: 'Date', value: billDate, align:'right'},
+    ] }
+]);
+const secondPart = receipt.create([
+
+   
+    { type: 'table', lines: items },
     { type: 'empty' },
-    { type: 'text', value: 'Some extra information to add to the footer of this docket.', align: 'center' },
-    { type: 'empty' },
+    {type: 'text', value:'User: '+userName, align:'left'},
+    { type: 'text', value:'Total =  '+Total, align: 'right' },
+    
+    { type: 'empty' },/*
     { type: 'properties', lines: [
         { name: 'GST (10.00%)', value: 'AUD XX.XX' },
         { name: 'Total amount (excl. GST)', value: 'AUD XX.XX' },
@@ -37,30 +32,61 @@ const output = receipt.create([
     { type: 'properties', lines: [
         { name: 'Amount Received', value: 'AUD XX.XX' },
         { name: 'Amount Returned', value: 'AUD XX.XX' }
-    ] },
+    ] },*/
     { type: 'empty' },
-    { type: 'text', value: 'Final bits of text at the very base of a docket. This text wraps around as well!', align: 'center', padding: 5 }
+    { type: 'text', value: 'Thank you...', align: 'center', padding: 5 }
 ]);
+
 
 const escpos = require('escpos');
 escpos.Network = require('escpos-network');
 
 
-const printerDevice = new escpos.Network('192.168.0.17');
+const printerDevice = new escpos.Network(printerIp);
 const printer = new escpos.Printer(printerDevice);
 
 
 printerDevice.open(function(error){
     printer
         .font('B')
-        .text(output).encode()
-    
-    
+        
+        .align("CT").style('B2').size(1,1).text(billHeader).control('VT').feed().control('CR')
+        .style('NORMAL')
+        .size('1')
+        .align('CT').text(billSubHeader1).control('CR')
+        .align('CT').text(billSubHeader2)
+        .align('LT')  
+        .size(1)        
+        .text(firstPart)
+        .align('CT')
+        .text(secondPart).encode()
+        
+        
+
     printer.feed();
     printer.feed();
-    printer.beep(1,100);
-    printer.cut();
+    printer.beep(5,100);
     printer.close();
 });
+}
 
-
+generateBill(
+    '1234',
+    '12/9/21',
+    [{ item: 'Product 1', qty: 1, cost: 1000 },
+    { item: 'Product 2 with a really long name', qty: 1, cost: 17500},
+    { item: 'Another product wth quite a name', qty: 2, cost: 900 },
+    { item: 'Product 4', qty: 1, cost: 80  },
+    { item: 'This length is ridiculously lengthy', qty: 14, cost: 8516 },
+    { item: 'Product 6', qty: 3, cost: 500 },
+    { item: 'Product 7', qty: 3, cost: 500 }],
+    'Fuad',
+    '1000.00',
+    '192.168.0.17',
+    'TABLE 123',
+    '',
+    ''
+    /*
+    'near Madeena Masjid, Chelakkad',
+    'MOb:789456123, 123456789'*/
+);
